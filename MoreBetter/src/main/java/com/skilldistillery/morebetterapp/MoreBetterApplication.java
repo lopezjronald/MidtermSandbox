4a1982948320fa415f2d5bcbf4c259bc37a563d1
Rich Wasek <rideburtonrw@gmail.com>
2020-05-21 11:28:42 -0600

    Stub Boot/MVC project

---
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/MoreBetterApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.morebetterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreBetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreBetterApplication.class, args);
+	}
+
+}
---
+++ MoreBetter/src/test/java/com/skilldistillery/morebetterapp/MoreBetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.morebetterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreBetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
