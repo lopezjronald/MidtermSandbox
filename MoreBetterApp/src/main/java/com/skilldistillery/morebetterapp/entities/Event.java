caad678ab0fa7e0a3aa96190e411aa9334a3cb6b
Rich Wasek <rideburtonrw@gmail.com>
2020-05-22 16:09:31 -0600

    Updated Event Entity/Test

---
+++ MoreBetterApp/src/main/java/com/skilldistillery/morebetterapp/entities/Event.java
@@ -0,0 +1,129 @@
+package com.skilldistillery.morebetterapp.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Event {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String title;
+	private String description;
+	private String location;
+	
+	@Column(name="event_date")
+	private String eventDate;
+	
+	@Column(name="max_capacity")
+	private Integer maxCapacity;
+	
+	private Double price;
+	
+	@Column(name="created_at")
+	private LocalDateTime created;
+	
+	@Column(name="last_updated")
+	private LocalDateTime updated;
+
+	
+	
+	
+	// constructors
+	public Event() {
+		super();
+	}
+	
+	
+	// methods
+	
+
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", title=" + title + ", description=" + description + ", location=" + location
+				+ ", eventDate=" + eventDate + ", maxCapacity=" + maxCapacity + ", price=" + price + ", created="
+				+ created + ", updated=" + updated + "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(String eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public Integer getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(Integer maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public LocalDateTime getCreated() {
+		return created;
+	}
+
+	public void setCreated(LocalDateTime created) {
+		this.created = created;
+	}
+
+	public LocalDateTime getUpdated() {
+		return updated;
+	}
+
+	public void setUpdated(LocalDateTime updated) {
+		this.updated = updated;
+	}
+	
+	
+}
---
+++ MoreBetterApp/src/test/java/com/skilldistillery/morebetterapp/entities/EventTest.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.morebetterapp.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Event event;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("MoreBetterPU");
+
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		event = em.find(Event.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		event = null;
+	}
+
+	@Test
+	@DisplayName("testing that event mappings work")
+	void test1() {
+
+		assertNotNull(event);
+//		assertEquals("Why financial literacy matters ", event.getTitle());
+//		assertEquals("Financial literacy cannot predict or remedy a crisis, but financial education will play a pivotal role in the economic recovery of our country.", event.getDescription());
+	}
+
+}
