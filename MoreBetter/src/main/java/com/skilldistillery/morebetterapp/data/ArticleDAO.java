3860e042c2f8081d384b09b0146712ae90e57d54
Rich Wasek <rideburtonrw@gmail.com>
2020-05-23 15:38:15 -0600

    Updated Category and Article DAOImpl methods

--- MoreBetter/src/main/java/com/skilldistillery/morebetterapp/data/ArticleDAO.java
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/data/ArticleDAO.java
@@ -5,7 +5,7 @@ import com.skilldistillery.morebetterapp.entities.Category;
 
 public interface ArticleDAO {
 	public Article findArticleById(int id);
-	public Article findArticleByTitle(String title);
+//	public Article findArticleByTitle(String title);
 //	public Article findArticleByCategory(Category category);
 	public Article createArticle(Article article);
 	// this delete may have to take in the mentor/user id?:
--- MoreBetter/src/main/java/com/skilldistillery/morebetterapp/data/ArticleDAOImpl.java
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/data/ArticleDAOImpl.java
@@ -17,32 +17,42 @@ public class ArticleDAOImpl implements ArticleDAO {
 	
 	@Override
 	public Article findArticleById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return em.find(Article.class, id);
 	}
 
-	@Override
-	public Article findArticleByTitle(String title) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	@Override
+//	public Article findArticleByTitle(String title) {
+//		return null;
+//	}
 
 	@Override
 	public Article createArticle(Article article) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(article);
+		em.flush();
+		return article;
 	}
 
 	@Override
 	public boolean deleteArticleById(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		Article article = null;
+		String jpql = "SELECT a FROM Article a WHERE a.id = :id";
+		article = em.createQuery(jpql, Article.class).setParameter("id", id).getSingleResult();
+		em.remove(article);
+		boolean stillContains = !em.contains(article);
+		em.flush();
+		return stillContains; // returns TRUE if deleted successfully
 	}
 
 	@Override
 	public Article updateArticle(int id, Article article) {
-		// TODO Auto-generated method stub
-		return null;
+		Article updatedArticle = em.find(Article.class, id);
+		updatedArticle.setCategory(article.getCategory());
+		updatedArticle.setUserAuthor(article.getUserAuthor());
+		updatedArticle.setTitle(article.getTitle());
+		updatedArticle.setContents(article.getContents());
+		updatedArticle.setCreated(article.getCreated());
+		em.flush();
+		return updatedArticle;
 	}
 
 }
