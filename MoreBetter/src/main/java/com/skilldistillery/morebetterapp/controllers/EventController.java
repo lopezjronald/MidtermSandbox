5e8911a290e868dbfeb2b869da2ceef80ff3d8b3
Rich Wasek <rideburtonrw@gmail.com>
2020-05-23 11:30:24 -0600

    Stubbed out EventController

---
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/controllers/EventController.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.morebetterapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventController {
+
+//	@RequestMapping(path= )
+//	public String eventPage() {
+//		return "";
+//	}
+}
