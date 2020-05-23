c7dfaaadb919a7aafdc980074093cedc4d7f2f18
Rich Wasek <rideburtonrw@gmail.com>
2020-05-23 11:55:05 -0600

    Category DAO/DAOImpl/Controller stubbed out

---
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/controllers/CategoryController.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.morebetterapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CategoryController {
+
+//	@RequestMapping(path= )
+//	public String categoryPage() {
+//		return "";
+//	}
+}
