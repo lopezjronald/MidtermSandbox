0dd45cd8408c8e846e61ffb24ff53b59cbbf1848
Rich Wasek <rideburtonrw@gmail.com>
2020-05-23 11:45:40 -0600

    Stubbed out Article Controller

---
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/controllers/ArticleController.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.morebetterapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ArticleController {
+
+//	@RequestMapping(path= )
+//	public String articlePage() {
+//		return "";
+//	}
+}
